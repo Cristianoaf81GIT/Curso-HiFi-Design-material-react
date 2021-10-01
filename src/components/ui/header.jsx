d9e51318f8985ebe4fef45cb8a8f9c0669296c16
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2021-10-01 08:19:44 -0300

    class(21): tabs overview

--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import ToolBar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/styles';
+
+import logo from '../../assets/logo.svg';
 
 
 function ElevationScroll(props) {
@@ -17,14 +20,29 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles(theme => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: "3em"
+  },
+  logo: {
+    height: "7em"
+  }
+}))
+
 export function Header(props) {
+  const classes = useStyles();
+
   return(
+    <React.Fragment>
     <ElevationScroll>
     <AppBar position="fixed">
-      <ToolBar>
-        Arc development
-      </ToolBar>
+      <ToolBar disableGutters>
+        <img src={logo} alt="company logo" className={classes.logo}/>
+      </ToolBar>    
     </AppBar>
     </ElevationScroll>
+    <div className={classes.toolbarMargin}/>
+    </React.Fragment>
   );
 }
--- src/components/ui/theme.js
+++ src/components/ui/theme.js
@@ -2,4 +2,26 @@ import { createTheme } from '@material-ui/core/styles';
 //https://v4.mui.com/pt/customization/palette/
 //https://v4.mui.com/pt/customization/default-theme/?expand-path=$.palette
 
-export default createTheme({}); 
+const arcBlue = "#0B72B9";
+const arcOrange = "#FFBA60";
+
+export default createTheme({
+  palette: {
+    common: {
+      arcBlue: `${arcBlue}`,
+      arcOrange: `${arcOrange}`,
+    },
+    primary: {
+      main: `${arcBlue}`
+    },
+    secondary: {
+      main: `${arcOrange}`
+    }
+  },
+  typography: {
+    h3: {
+      fontWeight: 300,
+    },
+    
+  }
+}); 

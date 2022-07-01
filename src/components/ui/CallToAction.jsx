c627b6c31d242c11fd52e4928f7cabb12e311243
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2022-07-01 08:34:41 -0300

    77. Creating The Call To Action Continued01

--- src/components/ui/CallToAction.jsx
+++ src/components/ui/CallToAction.jsx
@@ -4,6 +4,8 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { makeStyles, createStyles, useTheme } from "@material-ui/core/styles";
 import ButtonArrow from "./ButtonArrow";
+import background from "../../assets/background.jpg";
+import mobileBackground from "../../assets/mobileBackground.jpg";
 
 const useStyles = makeStyles((theme) => createStyles({
   learnButton: {
@@ -15,14 +17,40 @@ const useStyles = makeStyles((theme) => createStyles({
       marginBottom: "2em"
     }
   },
+  background: {
+    backgroundImage: `url(${background})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '60em',
+    width: '100%',
+    [theme.breakpoints.down('md')]: {
+      backgroundImage: `url(${mobileBackground})`,
+    }
+  },
+  estimateButton: {
+    ...theme.typography.estimate,
+    borderRadius: 50,
+    height:80,
+    width: 205,
+    backgroundColor: theme.palette.common.arcOrange,
+    fontSize: "1.5rem",
+    marginRight: "5em"
+  }
 }));
 
 export default function CallToAction() {
   const classes = useStyles();
   const theme = useTheme();
 
-  return <Grid container>
-    <Grid item>
+  return <Grid 
+      container 
+      // style={{ height: "60em" }} 
+      alignItems="center" 
+      justifyContent="space-between"
+      className={classes.background}
+    >
+    <Grid item style={{ marginLeft: "5em"}}>
       <Grid container direction="column">
         <Grid item>
           <Typography variant="h2">
@@ -30,7 +58,7 @@ export default function CallToAction() {
             <br/> 
             Revolutionary results.
           </Typography>
-          <Typography variant="subtitle2">
+          <Typography variant="subtitle2" style={{ fontSize: "1.5rem" }}>
             Take advantage of the 21st century.
           </Typography>
           <Grid container item>
@@ -45,5 +73,9 @@ export default function CallToAction() {
         </Grid>
       </Grid>
     </Grid>
+    <Grid item >
+      <Button variant="contained" className={classes.estimateButton}>Free Estimate</Button>
+    </Grid>
+    
   </Grid>
 }

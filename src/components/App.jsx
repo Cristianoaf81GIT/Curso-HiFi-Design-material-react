4d9c9f75b0813683f6126eab29143bcb99f46a55
Cristiano Alexandre de Faria <cristianoaf81@hotmail.com>
2022-08-07 11:01:22 -0300

    83. Services Page - Setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {Header} from './ui/header';
 import  Footer  from './ui/footer';
+import Services from './Services';
 import LandingPage from '../components/LandingPage';
 import {ThemeProvider} from '@material-ui/styles';
 import theme from './ui/theme';
@@ -23,7 +24,16 @@ function App() {
         <Route exact path="/" render={
           (props) => <LandingPage {...props} setValue={setValue} setSelectedIndex={setSelectedIndex}/>
         }/>
-        <Route exact path="/services" component={() => <div>Services</div>}/>
+        <Route 
+          exact 
+          path="/services" 
+          render={
+            (props) => <Services 
+                          {...props} 
+                          setValue={setValue} 
+                          setSelectedIndex={setSelectedIndex} />
+         }
+        />
         <Route exact path="/customsoftware" component={() => <div>Custom Software</div>}/>
         <Route exact path="/mobileapps" component={() => <div>Mobile apps</div>}/>
         <Route exact path="/websites" component={() => <div>Websites</div>}/>

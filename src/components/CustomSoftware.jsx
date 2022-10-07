a3298339e3ec07471d2322554cfab8fd2ab8c77b
Cristiano Alexandre de Faria <cristianoaf81@hotmail.com>
2022-10-07 07:31:37 -0300

    88. Custom Software Development Page - Animations Row 1

--- src/components/CustomSoftware.jsx
+++ src/components/CustomSoftware.jsx
@@ -12,6 +12,8 @@ import forwardArrow from '../assets/forwardArrow.svg';
 import lighBulb from '../assets/bulb.svg';
 import cash from '../assets/cash.svg';
 import stopwatch from '../assets/stopwatch.svg';
+import documentsAnimation from '../animations/documentsAnimation/data';
+import scaleAnimation from '../animations/scaleAnimation/data.json'
 
 const useStyles = makeStyles(theme => createStyles({
   heading: {
@@ -26,6 +28,9 @@ const useStyles = makeStyles(theme => createStyles({
     paddingTop: "2em",
     paddingBottom: "10em"
   },
+  itemContainer: {
+    maxWidth: "40em"
+  }
 }));
 
 
@@ -33,6 +38,24 @@ const useStyles = makeStyles(theme => createStyles({
 export default function CustomSoftware(props) {
     const classes = useStyles();
 
+    const documentsOptions = {
+      loop: true,
+        autoplay: false, 
+        animationData: documentsAnimation,
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice'
+        }
+    }
+
+
+    const scaleOptions = {
+      loop: true,
+        autoplay: false, 
+        animationData: scaleAnimation,
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice'
+        }
+    }
 
     return(
         <Grid container direction="column" className={classes.mainContainer}>
@@ -163,6 +186,83 @@ export default function CustomSoftware(props) {
               </Grid>
 
             </Grid>
+          
+          <Grid item container direction="row">
+              
+              <Grid item container className={classes.itemContainer}>
+                
+                <Grid item container direction="column" md>
+                  <Grid item>
+                    <Typography variant="h4">
+                      Digital documents & Data
+                    </Typography>  
+                  </Grid>  
+
+                  <Grid item>
+                    <Typography variant="body1" paragraph>
+                      Reduce Errors. Reduce Waste. Reduce Costs.
+                    </Typography>
+                    <Typography variant="body1"  paragraph>
+                      Billions are spent annually on the purchasing, printing, and
+                      distribution of paper. On top of the massive environmental
+                      impact this has, it causes harm to your bottom line as well.
+                    </Typography>
+                    <Typography variant="body1"  paragraph>
+                      By utilizing digital forms and documents you can remove these
+                      obsolete expenses, accelerate your communication, and help the
+                      Earth.
+                    </Typography>
+                  </Grid>
+                </Grid> 
+
+                <Grid item md>
+                  <Lottie 
+                    options={documentsOptions} 
+                    isStopped={false} 
+                    style={{
+                      maxHeight: 325,
+                      maxWidth: 275,
+                      minHeight: 275
+                    }}
+                  />
+                </Grid>
+
+              </Grid>
+
+              <Grid item container className={classes.itemContainer}>
+
+              <Grid item md>
+                  <Lottie 
+                    options={scaleOptions}  
+                    isStopped={false}                    
+                    style={{
+                      maxHeight: 260,
+                      maxWidth: 289,                      
+                    }}
+                  />
+                </Grid>
+                
+                <Grid item container direction="column" md>
+                  <Grid item>
+                    <Typography variant="h4" align="right">
+                      Scale
+                    </Typography>  
+                  </Grid>  
+
+                  <Grid item>
+                    <Typography variant="body1" paragraph align='right'>
+                    Whether you’re a large brand, just getting started, or taking
+                    off right now, our application architecture ensures pain-free
+                    growth and reliability.
+                    </Typography>
+                    
+                  </Grid>
+                </Grid>               
+
+              </Grid>
+
+          </Grid>
+
         </Grid>
     );
 

b25689fd9434e000fe8ad5d6e12676020c2a7af5
cristiano alexandre de faria <cristianoaf81@gmail.com>
2023-08-08 13:44:36 -0300

    111. Using TextFields - State Controlled Values

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
-import {makeStyles, useTheme} from '@material-ui/core/styles';
+import {makeStyles, useTheme } from '@material-ui/core/styles';
+import {useMediaQuery} from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
@@ -8,6 +9,10 @@ import TetField from '@material-ui/core/TextField';
 import background from '../assets/background.jpg';
 import phoneIcon from '../assets/phone.svg';
 import emailIcon from '../assets/email.svg';
+import { TextField } from '@material-ui/core';
+import airplane from '../assets/send.svg';
+import ButtonArrow from '../components/ui/ButtonArrow';
+
 
 const useStyles = makeStyles(theme => ({
   background: {
@@ -17,11 +22,43 @@ const useStyles = makeStyles(theme => ({
     backgroundRepeat: 'no-repeat',
     height: '60em',    
   },
+  estimateButton: {
+    ...theme.typography.estimate,
+    borderRadius: 50,
+    height: 80,
+    width: 205,
+    backgroundColor: theme.palette.common.arcOrange,
+    fontSize: '1.5rem',
+    marginRight: '5em',
+    marginLeft: '2em',
+    [theme.breakpoints.down('sm')]: {
+      marginRight: 0,
+      marginLeft: 0,
+    },
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.light,
+    },
+  },
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: '0.7rem',
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '2em',
+    },
+  },
 }));
 
 export default function Contact(props) {
   const classes = useStyles();
   const theme = useTheme();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [message, setMessage] = useState('');
+  const matchesSm = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <Grid container direction='row'>
@@ -59,8 +96,99 @@ export default function Contact(props) {
           </Typography>
         </Grid>
       </Grid>
-      <Grid item container className={classes.background} lg={9}>
-  
+      <Grid item container>
+        <Grid item>
+          <TextField 
+            label="Name" 
+            id="name" value={name} 
+            onChange={(event) => { setName(event.target.value); } }
+          />
+        </Grid>
+        <Grid item>
+           <TextField 
+            label="Email" 
+            id="email" 
+            value={email}
+            onChange={(event) => { setEmail(event.target.value); } }
+          />
+        </Grid>
+        <Grid item> 
+          <TextField 
+            label="Phone" 
+            id="phone" 
+            value={phone}
+            onChange={(event) => { setPhone(event.target.value); }}
+          />
+        </Grid>
+      </Grid>
+      <Grid item>
+        <TextField 
+          value={message}
+          multiline
+          maxRows={10}
+          id="message"
+          onChange={(event) => { setMessage(event.target.value); } }
+        />
+      </Grid>
+      <Grid item>
+        <Button variant="contained">
+          Send message
+          <img src={airplane} alt="paper airplane"/>
+        </Button>
+      </Grid>
+      <Grid item container className={classes.background} lg={9} alignItems='center'>
+        <Grid
+          item
+          style={{
+          marginLeft: matchesSm ? 0 : '5em',
+          textAlign: matchesSm ? 'center' : 'inherit',
+        }}
+        >
+        <Grid container direction="column">
+          <Grid item>
+            <Typography variant="h2">
+              Simple Software.
+              <br />
+              Revolutionary results.
+            </Typography>
+            <Typography variant="subtitle2" style={{ fontSize: '1.5rem' }}>
+              Take advantage of the 21st century.
+            </Typography>
+            <Grid
+              container
+              item
+              justifyContent={matchesSm ? 'center' : undefined}
+            >
+              <Button
+                variant="outlined"
+                className={classes.learnButton}
+                component={Link}
+                to="/revolution"
+                onClick={() => props.setValue(2)}
+              >
+                <span style={{ marginRight: 5 }}>Learn More</span>
+                <ButtonArrow
+                  width={10}
+                  height={10}
+                  fill={theme.palette.common.arcBlue}
+                />
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          className={classes.estimateButton}
+          component={Link}
+          to="/estimate"
+          onClick={() => props.setValue(5)}
+        >
+          Free Estimate
+        </Button>
+      </Grid>
+ 
       </Grid>
 
     </Grid>
